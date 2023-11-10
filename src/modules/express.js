efee33c8157dbb702d786f236dc22116b45db5d1
Arthur Santos <santos.santos86@hotmail.com>
2023-11-10 20:08:24 -0300

    config cors

--- src/modules/express.js
+++ src/modules/express.js
@@ -12,6 +12,8 @@ connectToDatabase();
 app.use(express.json());
 app.use(cors());
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
